7643e62d8b4c0600f759e3dba5a493b047aec540
rohitpjpti18 <rprajapati@deqode.com>
2022-04-06 11:39:28 +0530

    initial commit

---
+++ src/main/java/com/deqode/practice/repository/CustomItemRepository.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.deqode.practice.repository;
+
+/**
+ * @author rohit
+ *
+ */
+public interface CustomItemRepository {
+	
+	void updateItemQuantity(String itemName, float newQuantity);
+
+}
---
+++ src/main/java/com/deqode/practice/repository/ItemRepository.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.deqode.practice.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import com.deqode.practice.model.GroceryItem;
+
+/**
+ * @author rohit
+ *
+ */
+public interface ItemRepository extends MongoRepository<GroceryItem, String> {
+	@Query("{name:'?0'}")
+	GroceryItem findItemByName(String name);
+	
+	@Query(value="{category:'?0'}", fields="{'name' : 1, 'quantity' : 1}")
+	List<GroceryItem> findAll(String category);
+	
+	public long count();
+
+}
